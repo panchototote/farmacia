2db36408be6c521ad55466eb7c3cf75220da4342
panchototote <ffuenzalida90@gmail.com>
2021-12-03 19:44:31 -0300

    commit segundo

---
+++ frontend/src/App.js
@@ -0,0 +1,52 @@
+import data from './data';
+
+function App() {
+  return (
+    <div className="grid-container">
+        <header className="row">
+            <div>
+                <a className="brand" href="index.html">Tu-Farmacia</a>
+            </div>
+            <div>
+                <a href="cart.html">Carro</a>
+                <a href="signin.html">Entrar</a>
+            </div>
+        </header>
+        <main>
+            <div className="row center">
+              {
+                data.products.map((products) =>(
+                  <div key={products._id} className="card">
+                    <a href={`/product/${products._id}`}>
+                        <img className="medium" src={`./imagenes/${products.foto}`} alt={products.name}/>
+                    
+                    <div className="card-body">
+                            <h2>{products.name}</h2>
+                        <div className="principio">
+                            <span>
+                                <dl>
+                                  <dt>{products.principio}</dt>
+                                  <dt>{products.categoria}</dt>
+                                  <dt>{products.formato}</dt>
+                                </dl> 
+                            </span>        
+                        </div>
+                        <div className="price">
+                            <i className="fas fa-dollar-sign"> </i> {products.precio}               
+                        </div>
+                    </div>                    
+                    </a>
+                </div>   
+                ))
+              }
+                             
+            </div>
+        </main>
+        <footer className="row center">
+            Derecho Reservados
+        </footer>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/data.js
@@ -0,0 +1,80 @@
+const data= {
+    products:[
+        {            
+        _id:'1',
+        name:'Naproxeno de Sodio',
+        principio:'Naproxeno',
+        bioequivalente:'true',
+        foto:'producto1.png',
+        tiporeceta:'ninguna',
+        formato:'14 comprimidos',
+        categoria:'Analgésico',
+        codigobarra:'7805684515223',
+        laboratorio:'Laboratorio Chile',
+        Descripcion:'Antinflamatorio no esteroideo que se emplea en el tratamiento del dolor leve a moderado, la fiebre, la inflamación y la rigidez provocados por afecciones como artrosis, artritis psoriásica, espondilitis anquilosante, tendinitis y bursitis.',
+        precioPastilla:'240',
+        precio:'3360'
+    },
+    {            
+        _id:'2',
+        name:'Hipoglucin',
+        principio:'Metformina',
+        bioequivalente:'true',
+        foto:'producto1.png',
+        tiporeceta:'simple',
+        formato:'60 comprimidos',
+        categoria:'Diabetes',
+        codigobarra:'7805684430244',
+        laboratorio:'Laboratorio Chile',
+        Descripcion:'Antinflamatorio no esteroideo que se emplea en el tratamiento del dolor leve a moderado, la fiebre, la inflamación y la rigidez provocados por afecciones como artrosis, artritis psoriásica, espondilitis anquilosante, tendinitis y bursitis.',
+        precioPastilla:'425',
+        precio:'25499'
+    },
+    {            
+        _id:'3',
+        name:'Nastizol',
+        principio:'Clorfenamina, Pseudofedrina',
+        bioequivalente:'true',
+        foto:'producto1.png',
+        tiporeceta:'ninguna',
+        formato:'14 comprimidos',
+        categoria:'Descongestionantes',
+        codigobarra:'7805682591500',
+        laboratorio:'Bagó',
+        Descripcion:'Tratamiento sintomático de la rinitis alérgica estacional y crónica. Molestias del tracto respiratorio superior ocasionadas por el resfrío común.',
+        precioPastilla:'504',
+        precio:'10359'
+    },
+    {            
+        _id:'4',
+        name:'Sodio-Nax',
+        principio:'Naproxeno',
+        bioequivalente:'true',
+        foto:'producto1.png',
+        tiporeceta:'simple',
+        formato:'14 comprimidos',
+        categoria:'Analgésico',
+        codigobarra:'7805684515223',
+        laboratorio:'Laboratorio Chile',
+        Descripcion:'Antinflamatorio no esteroideo que se emplea en el tratamiento del dolor leve a moderado, la fiebre, la inflamación y la rigidez provocados por afecciones como artrosis, artritis psoriásica, espondilitis anquilosante, tendinitis y bursitis.',
+        precioPastilla:'240',
+        precio:'3360'
+    },
+    {            
+        _id:'5',
+        name:'Naproxeno',
+        principio:'Naproxeno',
+        bioequivalente:'true',
+        foto:'producto1.png',
+        tiporeceta:'simple',
+        formato:'14 comprimidos',
+        categoria:'Analgésico',
+        codigobarra:'7805684515223',
+        laboratorio:'Laboratorio Chile',
+        Descripcion:'Antinflamatorio no esteroideo que se emplea en el tratamiento del dolor leve a moderado, la fiebre, la inflamación y la rigidez provocados por afecciones como artrosis, artritis psoriásica, espondilitis anquilosante, tendinitis y bursitis.',
+        precioPastilla:'240',
+        precio:'3360'
+    }
+    ]
+}
+export default data;
